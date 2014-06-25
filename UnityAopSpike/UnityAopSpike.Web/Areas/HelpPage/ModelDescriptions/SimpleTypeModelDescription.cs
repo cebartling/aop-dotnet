d32897e0aae62e313b477ba32cacbf34954b290e
Christopher Bartling <chris.bartling@gmail.com>
2014-06-25 18:53:14 -0500

    Created project.

---
+++ UnityAopSpike/UnityAopSpike.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace UnityAopSpike.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ UnityAopSpike/UnityAopSpike.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UnityAopSpike.Web.WebApiApplication" Language="C#" %>
