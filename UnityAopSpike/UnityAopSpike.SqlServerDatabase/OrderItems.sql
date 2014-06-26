7272fb413db5b9492fa32522768a2b14c3ccdbbf
Christopher Bartling <chris.bartling@gmail.com>
2014-06-26 16:14:56 -0500

    Added order items.

---
+++ UnityAopSpike/UnityAopSpike.SqlServerDatabase/OrderItems.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[OrderItems]
+(
+    [Id] INT NOT NULL PRIMARY KEY, 
+    [order_id] INT NOT NULL, 
+    [quantity] SMALLINT NOT NULL, 
+    CONSTRAINT [FK_OrderItems_ToOrders] FOREIGN KEY ([order_id]) REFERENCES [Orders]([Id]) 
+)
